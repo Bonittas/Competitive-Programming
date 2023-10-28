9135a7466156054da5a5a50f1fff163fafcfa21a
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-10-28 10:24:30 +0300

    Create Array With Elements Not Equal to Average of Neighbors.js

---
+++ Array With Elements Not Equal to Average of Neighbors.js
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rearrangeArray(self, nums):
+        for i in range(1, len(nums) -1):
+            if (nums[i-1] < nums[i]< nums[i+1]) or (nums[i-1] > nums[i] >nums[i+1]):
+                nums[i+1], nums[i] = nums[i], nums[i+1]
+        return nums
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
